786478056d05164c6db1de6434ce0564c106133f
Sharma v <Sharma v>
2016-11-13 15:36:58 -0500

    Shubham update blog

--- public/js/emailme.js
+++ public/js/emailme.js
@@ -9,6 +9,6 @@ var lat = $("#lat").val();
 var date = dateAndTime;
 $.post( "/send/", { email: email, message: message, long: long, lat: lat, date: date})
   .done(function( data ) {
-    $( "#returnmessage" ).append('<div id="commenterdiv"><p id="commentername">Owner of this blog, Shubham Yeole (Graduate student in computer science major) received an email from '+email+' at '+date+'</p><p id="commentptag">Sample of your email: '+message+'</p></div><br>').addClass("animated slideInDown");
+    $( "#returnmessage" ).append('<div style="font-weight: bold; background-color: #f9f7f8; padding: 2%; color: #3F51B5">Email sent to Shubham Yeole. (Owner of this blog, Grad Student) from '+email+' on '+date+'<br><br><b style="color: red;">Please check your email for more information.</b><br><br>Sample of your email: '+message+'<br><br>').addClass("animated slideInDown");
   });
 });
