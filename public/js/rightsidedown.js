7b903dc8d55fe60994943df63048442ebbda47d4
Sharma v <Sharma v>
2016-11-14 08:47:37 -0500

    Shubham update blog

--- public/js/rightsidedown.js
+++ public/js/rightsidedown.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
 })
 $(document).on("click",".w3-padding-16",function() { 
 	  	var id = $(this).attr('id');
-	 	window.location = "http://localhost:3000/dashboard/"+id;
+	 	window.location = "https://java-nodejs-blog.herokuapp.com/dashboard/"+id;
 	 	// window.location = "https://java-nodejs-blog.herokuapp.com/dashboard/"+id;
 });
